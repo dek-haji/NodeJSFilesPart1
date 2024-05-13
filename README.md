ed706f132341c9a71ca5af279a9be5bf964b56f7
dek.haji1@gmail.com <dek.haji1@gmail.com>
2024-05-13 16:23:07 -0500

    introducing NextJS

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# NodeJSFilesPart1
+#NodeJs Tutorial 
+ Things that we are going to learn in this file:
+ What is NodeJs?
+ NodeJs is a runtime environment for executing JavaScript code outside of a browser.
+NodeJs is built on the V8 JavaScript engine which is the same engine that powers Google Chrome.
+
+
--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const http = require('http');
-const routes = require('./routes');
-const server = http.createServer(routes.handler);
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('The middleware!');
+  next(); //Allows the request to continue to the next middleware in line
+});
+app.use((req, res, next) => {
+    console.log('Another the middleware!');
+    res.send('<h1>Hello from Express!</h1>');
+});
+const server = http.createServer(app);
 server.listen(3000);
