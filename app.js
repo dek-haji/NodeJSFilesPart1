1ff4e2fb9380b1e1c69d154cdb186cb4506d9339
dek.haji1@gmail.com <dek.haji1@gmail.com>
2024-05-14 15:56:13 -0500

    added middleware

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
 const app = express();
-
-app.use((req, res, next) => {
-  console.log('The middleware!');
-  next(); //Allows the request to continue to the next middleware in line
-});
-app.use((req, res, next) => {
+// this is how we calling different routes using express.
+app.use('/properties',(req, res, next) => {
     console.log('Another the middleware!');
-    res.send('<h1>Hello from Express!</h1>');
+    res.send('<h1>Property list!</h1>');
 });
-const server = http.createServer(app);
-server.listen(3000);
+//middleware is a function that gets executed for every incoming request.
+app.get('/', (req, res, next) => { 
+    res.send('<h1>Home page!</h1>');
+});
+app.listen(3000);
